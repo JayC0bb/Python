b2a08a9cff61686a870d16a23a0a71e3e96bfb62
JayC0bb <76946921+JayC0bb@users.noreply.github.com>
2022-04-11 08:34:09 +0200

    Update Adresace.py

--- Adresace.py
+++ Adresace.py
@@ -1,10 +1,10 @@
 import ipaddress
 
-sit = input("Síť:")
+ sit = input("Síť:")
 IPadresa = ipaddress.IPv4Network(sit)
-print("Maska:", IPadresa.netmask)
-print("Network:", IPadresa.network_address)
-print("Broadcast:", IPadresa.broadcast_address)
+ print("Maska:", IPadresa.netmask)
+ print("Network:", IPadresa.network_address)
+ print("Broadcast:", IPadresa.broadcast_address)
 adres = IPadresa.num_addresses
-print("Adresy:", adres)
-print("Hosti:", adres-2)
+ print("Adresy:", adres)
+ print("Hosti:", adres-2)
